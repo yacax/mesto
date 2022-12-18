71697761d3ea839b0683bb837daac3bf13829ce2
yacax <yacax@yandex.ru>
2022-12-18 23:16:57 +0300

    устранить замечания

--- scripts/index.js
+++ scripts/index.js
@@ -2,21 +2,16 @@ let popup = document.querySelector('.popup');
 
 let editProfileButton = document.querySelector('.profile__edit-button');
 let savePopupButton = popup.querySelector('.popup__save-button');
-let closePopupButton = popup.querySelector('.popup__close-button');
+let closePopupSubmit = popup.querySelector('.popup__window');
 
-let profileTitleInput;
-let profileSubtitleInput;
+let profileTitleInput  = popup.querySelector('.popup__text-input_name_title');
+let profileSubtitleInput = popup.querySelector('.popup__text-input_name_subtitle');
 
 let profile = document.querySelector('.profile');
 let profileTitle = profile.querySelector('.profile__title');
 let profileSubtitle = profile.querySelector('.profile__subtitle');
 
-
-
 function openPopup() {
-  profileTitleInput = popup.querySelector('.popup__text-input_name_title');
-  profileSubtitleInput = popup.querySelector('.popup__text-input_name_subtitle');
-
   profileTitleInput.value = profileTitle.textContent;
   profileSubtitleInput.value = profileSubtitle.textContent;
 
@@ -44,4 +39,4 @@ editProfileButton.addEventListener('click', openPopup);
 
 savePopupButton.addEventListener('click', handleFormSubmit);
 
-closePopupButton.addEventListener('click', closePopup)
+closePopupSubmit.addEventListener('submit', closePopup);
